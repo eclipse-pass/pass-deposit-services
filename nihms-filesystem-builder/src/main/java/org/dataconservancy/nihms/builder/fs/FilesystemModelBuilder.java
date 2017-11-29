e59dc061446f0d3f3d1abba4f6b340669b7783f3
Jim Martino <james.r.martino@gmail.com>
2017-11-29 09:01:32 -0500

    add more cases to switch in FilesystemModelBuilder

--- nihms-filesystem-builder/src/main/java/org/dataconservancy/nihms/builder/fs/FilesystemModelBuilder.java
+++ nihms-filesystem-builder/src/main/java/org/dataconservancy/nihms/builder/fs/FilesystemModelBuilder.java
@@ -26,12 +26,15 @@ import org.dataconservancy.nihms.model.NihmsSubmission;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
 import java.util.Enumeration;
 import java.util.Properties;
 
 /**
  * Builds a NIHMS submission from a file on a locally mounted filesystem.  The file contains key-value pairs that would
  * loosely mimic the form data provided by the view layer.
+ *
+ * This implementation is for a bare-bones demo only - in particular we can have just one Person, and just one File
  */
 public class FilesystemModelBuilder implements SubmissionBuilder {
 
@@ -42,6 +45,7 @@ public class FilesystemModelBuilder implements SubmissionBuilder {
         NihmsSubmission submission = new NihmsSubmission();
         NihmsFile file = new NihmsFile();
         NihmsMetadata metadata = new NihmsMetadata();
+        NihmsMetadata.Person person = new NihmsMetadata.Person();
 
         try {
             is = getClass().getClassLoader().getResourceAsStream(formDataUrl);
@@ -58,6 +62,12 @@ public class FilesystemModelBuilder implements SubmissionBuilder {
                 String value = properties.getProperty(key);
 
                 switch (key) {
+                    //the id for the submission
+                    case NihmsBuilderPropertyNames.NIHMS_SUBMISSION_ID:
+                        submission.setId(value);
+                        break;
+
+                    //file properties
                     case NihmsBuilderPropertyNames.NIHMS_FILE_NAME:
                         file.setName(value);
                         break;
@@ -65,16 +75,58 @@ public class FilesystemModelBuilder implements SubmissionBuilder {
                         file.setLabel(value);
                         break;
 
+                    //journal metadata
                     case NihmsBuilderPropertyNames.NIHMS_JOURNAL_ID:
+                        metadata.getJournalMetadata().setJournalId(value);
+                        break;
+                    case NihmsBuilderPropertyNames.NIHMS_JOURNAL_ISSN:
+                        metadata.getJournalMetadata().setIssn(value);
+                        break;
+                    case NihmsBuilderPropertyNames.NIHMS_JOURNAL_TITLE:
+                        metadata.getJournalMetadata().setJournalTitle(value);
+                        break;
 
+                    //manuscript metadata
+                    case NihmsBuilderPropertyNames.NIHMS_MANUSCRIPT_ID:
+                        metadata.getManuscriptMetadata().setNihmsId(value);
+                        break;
+                    case NihmsBuilderPropertyNames.NIHMS_MANUSCRIPT_PBMEDID:
+                        metadata.getManuscriptMetadata().setPubmedId(value);
+                        break;
+                    case NihmsBuilderPropertyNames.NIHMS_MANUSCRIPT_PUBMEDCENTRALID:
+                        metadata.getManuscriptMetadata().setPubmedCentralId(value);
+                        break;
+                    case NihmsBuilderPropertyNames.NIHMS_MANUSCRIPT_URL:
+                        metadata.getManuscriptMetadata().setManuscriptUrl(new URL(value));
+                        break;
 
-
+                    //person metadata
+                    case NihmsBuilderPropertyNames.NIHMS_PERSON_AUTHOR:
+                        person.setAuthor(Boolean.parseBoolean(value));
+                        break;
+                    case NihmsBuilderPropertyNames.NIHMS_PERSON_CORRESPONDINGPI:
+                        person.setCorrespondingPi(Boolean.parseBoolean(value));
+                        break;
+                    case NihmsBuilderPropertyNames.NIHMS_PERSON_EMAIL:
+                        person.setEmail(value);
+                        break;
+                    case NihmsBuilderPropertyNames.NIHMS_PERSON_FIRSTNAME:
+                        person.setFirstName(value);
+                        break;
+                    case NihmsBuilderPropertyNames.NIHMS_PERSON_MIDDLENAME:
+                        person.setMiddleName(value);
+                        break;
+                    case NihmsBuilderPropertyNames.NIHMS_PERSON_LASTNAME:
+                        person.setLastName(value);
+                        break;
                 }
-               // System.out.println("Key : " + key + ", Value : " + value);
             }
 
             //now populate the submission
             submission.getFiles().add(file);
+            metadata.getPersons().add(person);
+            submission.setMetadata(metadata);
+            submission.getManifest().getFiles().add(file);
 
         } catch (IOException ioe) {
             ioe.printStackTrace();
