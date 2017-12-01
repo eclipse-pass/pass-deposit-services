f47c399f910d20d7d9cb3e92f59d5791dc4fd697
Jim Martino <james.r.martino@gmail.com>
2017-12-01 15:23:05 -0500

    Initial CLI code - create module, application class, handle arguments and options, add maven dependency on args4j

---
+++ nihms-cli/src/main/java/org/dataconservancy/nihms/cli/NihmsSubmissionApp.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2017 Johns Hopkins University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.dataconservancy.nihms.cli;
+
+import java.io.File;
+
+import org.kohsuke.args4j.Argument;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+
+/**
+ * @author Jim Martino (jrm@jhu.edu)
+ */
+public class NihmsSubmissionApp {
+
+    /**
+     * Arguments - just the property file containing the submission elements
+     */
+    @Argument(required = true, index = 0, metaVar = "[properties file]", usage = "properties file for the submission")
+    public File propertiesFile = null;
+
+    /**
+     *
+     * General Options
+     */
+
+    /** Request for help/usage documentation */
+    @Option(name = "-h", aliases = { "-help", "--help" }, usage = "print help message")
+    public boolean help = false;
+
+    /** Requests the current version number of the cli application. */
+    @Option(name = "-v", aliases = { "-version", "--version" }, usage = "print version information")
+    public boolean version = false;
+
+    public NihmsSubmissionApp(){
+
+    }
+    public static void main(String[] args) {
+
+        final NihmsSubmissionApp application = new NihmsSubmissionApp();
+        CmdLineParser parser = new CmdLineParser(application);
+        //parser.setUsageWidth(80);
+
+        try {
+            parser.parseArgument(args);
+            /* Handle general options such as help, version */
+            if (application.help) {
+                parser.printUsage(System.err);
+                System.err.println();
+                System.exit(0);
+            } else if (application.version) {
+                System.err.println(NihmsCliException.class.getPackage()
+                        .getImplementationVersion());
+                System.exit(0);
+            }
+
+            /* Run the package generation application proper */
+            application.run();
+
+        } catch (CmdLineException e) {
+            /**
+             * This is an error in command line args, just print out usage data
+             * and description of the error.
+             */
+            System.err.println(e.getMessage());
+            parser.printUsage(System.err);
+            System.err.println();
+            System.exit(1);
+        } catch (NihmsCliException e) {
+            e.printStackTrace();
+            System.err.println(e.getMessage());
+            System.exit(1);
+        }
+    }
+
+    private void run() throws NihmsCliException {
+        //do the fun stuff here
+    }
+}
