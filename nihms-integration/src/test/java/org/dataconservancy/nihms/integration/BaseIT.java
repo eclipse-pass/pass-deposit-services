79b5e01dab48cadfcaaddf29800e7f71199e7e2a
Elliot Metsger <emetsger@gmail.com>
2018-05-07 17:16:46 -0400

    Indexer configuration, and smoke IT. (#48)

--- nihms-integration/src/test/java/org/dataconservancy/nihms/integration/BaseIT.java
+++ nihms-integration/src/test/java/org/dataconservancy/nihms/integration/BaseIT.java
@@ -20,7 +20,11 @@ import org.junit.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.concurrent.Callable;
+import java.util.function.Function;
+
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 public abstract class BaseIT {
 
@@ -33,4 +37,44 @@ public abstract class BaseIT {
         assertNotNull("Expected required system property 'docker.host.address' to be set.",
                 System.getProperty("docker.host.address"));
     }
+
+    public static <T> void attemptAndVerify(int times, Callable<T> callable, Function<T, Boolean> verification) {
+        if (times < 1) {
+            throw new IllegalArgumentException("times must be a positive integer");
+        }
+
+        long sleepMs = 2000;
+
+        Throwable throwable = null;
+
+        for (int i = 0; i < times; i++) {
+
+            T result = null;
+            try {
+                result = callable.call();
+            } catch (Exception e) {
+                throwable = e;
+            }
+
+            if (result != null) {
+                StringBuilder msg = new StringBuilder("Attempt failed");
+                if (throwable != null) {
+                    msg.append(" with: ").append(throwable.getMessage());
+                } else {
+                    msg.append("!");
+                }
+                assertTrue(msg.toString(), verification.apply(result));
+                break;
+            } else {
+                try {
+                    Thread.sleep(sleepMs);
+                } catch (InterruptedException ie) {
+                    Thread.interrupted();
+                    break;
+                }
+            }
+
+        }
+    }
+
 }
