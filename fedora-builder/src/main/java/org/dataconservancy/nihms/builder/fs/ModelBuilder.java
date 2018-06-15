0079bba7f06c7ca04ed74a381bd70f3014f8ffc2
Jim Martino <james.r.martino@gmail.com>
2018-06-15 17:00:45 -0400

    remove DepositFileLabelMaker class and unit terst from fedora-builder module

--- fedora-builder/src/main/java/org/dataconservancy/nihms/builder/fs/ModelBuilder.java
+++ fedora-builder/src/main/java/org/dataconservancy/nihms/builder/fs/ModelBuilder.java
@@ -353,7 +353,6 @@ abstract class ModelBuilder {
         ArrayList<DepositFile> files = new ArrayList<>();
         submission.setFiles(files);
         manifest.setFiles(files);
-        DepositFileLabelMaker labelMaker = new DepositFileLabelMaker();
 
         for (URI key : entities.keySet()) {
             PassEntity entity = entities.get(key);
