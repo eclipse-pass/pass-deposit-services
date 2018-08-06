c7e51b7bcdef1fd1215fb38c5587d1285d2eb46d
Elliot Metsger <emetsger@jhu.edu>
2018-08-06 16:06:43 -0400

    Rename id field on RepositoryConfig to key.

--- deposit-messaging/src/main/java/org/dataconservancy/pass/deposit/messaging/config/spring/DepositConfig.java
+++ deposit-messaging/src/main/java/org/dataconservancy/pass/deposit/messaging/config/spring/DepositConfig.java
@@ -229,7 +229,7 @@ public class DepositConfig {
         Map<String, Packager> packagers = repositories.keys().stream().map(repositories::getConfig)
                 .map(repoConfig -> {
                     return new Packager(
-                            repoConfig.getId(),
+                            repoConfig.getRepositoryKey(),
                             assemblers.get(repoConfig.getAssemblerConfig().getSpec()),
                             transports.get(repoConfig.getTransportConfig().getProtocolBinding().getProtocol()),
                             repoConfig,
