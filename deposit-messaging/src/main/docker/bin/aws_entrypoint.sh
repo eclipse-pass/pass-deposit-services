04b968a487ed530ea75cc8144882179ebdd090e3
Elliot Metsger <emetsger@gmail.com>
2019-02-20 09:31:52 -0500

    Build and push Docker images for oapass/deposit-services-core. (#197)
    * Build and push Docker images for oapass/deposit-services-core.
    - deposit-messaging builds the image
    - deposit-integration pushes during the deploy phase
    - if ITs fail deploy isn't executed
    - requires authentication credentials to be present in a <server> element in ~/.m2/settings.xml with an <id> of 'docker.io'
    
    * Correct the command used by the aws_entrypoint.sh

---
+++ deposit-messaging/src/main/docker/bin/aws_entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+aws s3 cp s3://pass-configuration-files/entrypoints/depositservices.sh /bin/depositservices_entrypoint.sh
+
+if [ -f /bin/depositservices_entrypoint.sh ]; then
+    echo "depositservices_entrypoint.sh found...running"
+    chmod 700 /bin/depositservices_entrypoint.sh
+    /bin/depositservices_entrypoint.sh
+else
+    java -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap org.springframework.boot.loader.JarLauncher listen
+fi
+
