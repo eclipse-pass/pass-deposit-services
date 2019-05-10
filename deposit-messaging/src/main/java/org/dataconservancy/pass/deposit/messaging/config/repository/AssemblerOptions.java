ef2179d06007a7f56bfbe8e1100e1317bee3c500
Elliot Metsger <emetsger@jhu.edu>
2019-05-10 09:19:17 -0400

    Support arbitrary JSON in AssemblerOptions.  Allows for Assembler implementations to include new configuration options without breaking serializations or requiring new methods on AssemblerOptions.

--- deposit-messaging/src/main/java/org/dataconservancy/pass/deposit/messaging/config/repository/AssemblerOptions.java
+++ deposit-messaging/src/main/java/org/dataconservancy/pass/deposit/messaging/config/repository/AssemblerOptions.java
@@ -15,6 +15,8 @@
  */
 package org.dataconservancy.pass.deposit.messaging.config.repository;
 
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
 import org.dataconservancy.pass.deposit.assembler.PackageOptions.Archive;
 import org.dataconservancy.pass.deposit.assembler.PackageOptions.Checksum;
 import org.dataconservancy.pass.deposit.assembler.PackageOptions.Compression;
@@ -26,6 +28,25 @@ import java.util.Objects;
 import java.util.stream.Collectors;
 
 /**
+ * Represents the {@code options} provided to an Assembler, typically serialized from JSON.  This class encapsulates
+ * the {@code options} object in the example serialization below:
+ * <pre>
+ *     "BagIt": {
+ *     "assembler": {
+ *       "specification": "simple",
+ *       "beanName": "simpleAssembler",
+ *       "options": {
+ *         "archive": "ZIP",
+ *         "compression": "NONE",
+ *         "algorithms": [
+ *           "sha512",
+ *           "md5"
+ *         ],
+ *         "baginfo-template-resource": "/bag-info.hbm"
+ *       }
+ *     }
+ * </pre>
+ *
  * @author Elliot Metsger (emetsger@jhu.edu)
  */
 public class AssemblerOptions {
@@ -35,6 +56,13 @@ public class AssemblerOptions {
     private String archive;
 
     private List<String> algorithms;
+
+    /*
+     * Carries additional Assembler options that are not defined as fields on this class.  Allows for arbitrary content
+     * to be added to the Assembler configuration in {@code repositories.json} without causing errors in serialization,
+     * and without adding new fields and accessors to this class.
+     */
+    private Map<String, Object> optionsMap = new HashMap<>();
 
     public String getCompression() {
         return compression;
@@ -60,6 +88,74 @@ public class AssemblerOptions {
         this.algorithms = algorithms;
     }
 
+    /**
+     * Includes only those configuration keys that are not explicitly defined (i.e. lack accessor methods) in this
+     * AssemblerOptions.  Given the following Assembler configuration in {@code repositories.json}:
+     * <pre>
+     *     "BagIt": {
+     *     "assembler": {
+     *       "specification": "simple",
+     *       "beanName": "simpleAssembler",
+     *       "options": {
+     *         "archive": "ZIP",
+     *         "compression": "NONE",
+     *         "algorithms": [
+     *           "sha512",
+     *           "md5"
+     *         ],
+     *         "baginfo-template-resource": "/bag-info.hbm"
+     *       }
+     *     }
+     * </pre>
+     *
+     * The {@code Map} returned by this method will only contain {@code baginfo-template-resource}.  The other
+     * properties present in the {@code options} JSON object have defined accessors in AssemblerOptions, so they are not
+     * included in the returned {@code Map}.  If <em>every</em> key is desired, see {@link #asOptionsMap()}.
+     *
+     * @return every key and value in the {@code options} JSON object that lacks accessor methods
+     * @see #getOptionsMap()
+     * @see #getAlgorithms()
+     * @see #getArchive()
+     * @see #getCompression()
+     */
+    @JsonAnyGetter
+    public Map<String, Object> getOptionsMap() {
+        return optionsMap;
+    }
+
+    @JsonAnySetter
+    public void add(String key, Object value) {
+        optionsMap.put(key, value);
+    }
+
+    /**
+     * Includes every configuration key in this AssemblerOptions as a {@code Map}.  This method differs from {@link
+     * #getOptionsMap()} by including <em>every</em> configuration key in the returned {@code Map}, while {@link
+     * #getOptionsMap()} only includes "extra" options.  Given the following Assembler configuration in {@code
+     * repositories.json}
+     * <pre>
+     *     "BagIt": {
+     *     "assembler": {
+     *       "specification": "simple",
+     *       "beanName": "simpleAssembler",
+     *       "options": {
+     *         "archive": "ZIP",
+     *         "compression": "NONE",
+     *         "algorithms": [
+     *           "sha512",
+     *           "md5"
+     *         ],
+     *         "baginfo-template-resource": "/bag-info.hbm"
+     *       }
+     *     }
+     * </pre>
+     *
+     * The {@code Map} returned by this method will contain every key and value in the {@code options} JSON object,
+     * including {@code baginfo-template-resource}.
+     *
+     * @return every key and value in {@code options} JSON object as a {@code Map}
+     * @see #getOptionsMap()
+     */
     public Map<String, Object> asOptionsMap() {
         return new HashMap<String, Object>() {
             {
@@ -75,6 +171,7 @@ public class AssemblerOptions {
                                     .map(algo -> Checksum.OPTS.valueOf(algo.toUpperCase()))
                                     .collect(Collectors.toList()));
                 }
+                this.putAll(optionsMap);
             }
         };
     }
