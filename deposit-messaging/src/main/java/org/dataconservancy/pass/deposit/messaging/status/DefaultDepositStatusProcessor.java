888ed82fcc585e03012b9e2d4b178446e7df11f9
Elliot Metsger <emetsger@jhu.edu>
2019-06-10 09:35:00 -0400

    Review logging in the o.d.p.deposit.messaging.status package.
    - Remove vestigial code

--- deposit-messaging/src/main/java/org/dataconservancy/pass/deposit/messaging/status/DefaultDepositStatusProcessor.java
+++ deposit-messaging/src/main/java/org/dataconservancy/pass/deposit/messaging/status/DefaultDepositStatusProcessor.java
@@ -15,8 +15,6 @@
  */
 package org.dataconservancy.pass.deposit.messaging.status;
 
-import org.dataconservancy.pass.deposit.messaging.config.repository.AuthRealm;
-import org.dataconservancy.pass.deposit.messaging.config.repository.BasicAuthRealm;
 import org.dataconservancy.pass.deposit.messaging.config.repository.RepositoryConfig;
 import org.dataconservancy.pass.deposit.messaging.config.repository.StatusMapping;
 import org.dataconservancy.pass.model.Deposit;
@@ -24,9 +22,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.net.URI;
-import java.util.Collection;
 import java.util.Map;
-import java.util.Optional;
 
 /**
  * Resolves the {@link Deposit#getDepositStatusRef() Deposit status reference}, parses the resolved document, and
@@ -94,15 +90,4 @@ public class DefaultDepositStatusProcessor implements DepositStatusProcessor {
         }
     }
 
-    private static Optional<BasicAuthRealm> matchRealm(String url, Collection<AuthRealm> authRealms) {
-        if (authRealms == null || authRealms.isEmpty()) {
-            return Optional.empty();
-        }
-
-        return authRealms.stream()
-                .filter(realm -> realm instanceof BasicAuthRealm)
-                .map(realm -> (BasicAuthRealm) realm)
-                .filter(realm -> url.startsWith(realm.getBaseUrl().toString()))
-                .findAny();
-    }
 }
